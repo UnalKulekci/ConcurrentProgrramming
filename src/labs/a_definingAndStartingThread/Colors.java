3c7356bea7f8e5af0b79c85d1e5e510154ef780e
ted <76978205+UnalKulekci@users.noreply.github.com>
2024-12-25 12:11:48 +0100

    Hands on Lab-A added(updated)

---
+++ src/labs/a_definingAndStartingThread/Colors.java
@@ -0,0 +1,9 @@
+package labs.a_definingAndStartingThread;
+
+public class Colors {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";  // Renkleri sıfırlar
+}
+
+
