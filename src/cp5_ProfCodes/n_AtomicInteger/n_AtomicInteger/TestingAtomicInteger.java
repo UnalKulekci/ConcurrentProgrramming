bf2d555c6700d41121defa058c5fb0740e947b73
ted <76978205+UnalKulekci@users.noreply.github.com>
2025-01-07 13:17:06 +0100

    fixed

--- src/cp5_ProfCodes/n_AtomicInteger/n_AtomicInteger/TestingAtomicInteger.java
+++ src/cp5_ProfCodes/n_AtomicInteger/n_AtomicInteger/TestingAtomicInteger.java
@@ -1,20 +1,21 @@
-package Concurrency_HighLevel._CourseExamples.n_AtomicInteger;
+package cp5_ProfCodes.n_AtomicInteger.n_AtomicInteger;
+
 
 public class TestingAtomicInteger {
     public static void main(String[] args) {
-        Counter simpleCounter = new Counter();
+        Counter2 simpleCounter = new Counter2();
         simpleCounter.decrement();
         simpleCounter.decrement();
         simpleCounter.increment();
         System.out.println(simpleCounter + " value: " + simpleCounter.value());
 
-        SynchronizedCounter synchronizedCounter = new SynchronizedCounter();
+        SynchronizedCounter2 synchronizedCounter = new SynchronizedCounter2();
         synchronizedCounter.decrement();
         synchronizedCounter.decrement();
         synchronizedCounter.increment();
         System.out.println(synchronizedCounter + " value: " + synchronizedCounter.value());
 
-        AtomicCounter atomicCounter = new AtomicCounter();
+        AtomicCounter2 atomicCounter = new AtomicCounter2();
         atomicCounter.decrement();
         atomicCounter.decrement();
         atomicCounter.increment();
